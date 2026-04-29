f21e5a244b9b72931211e9640fd8eaa350b04c3e
roadArtyom <roadarman@gmail.com>
2026-04-29 16:38:47 +0400

    fix

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -11,7 +11,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class PostFactory extends Factory
 {
     protected $model = Post::class;
-
     public function definition(): array
     {
         $type = fake()->randomElement(['video', 'image']);
